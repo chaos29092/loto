014c2aea3275c3f1a7fa503b29a88b6a8502b921
chaos29092 <chaos29092@gmail.com>
2016-09-28 09:37:43 +0800

    add es language

--- resources/lang/es/keywords.php
+++ resources/lang/es/keywords.php
@@ -8,44 +8,18 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'index_title' => 'Hornos de alta temperatura de laboratorio',
-    'index_description' => 'Hornos de Laboratorio de Alta Temperatura. Proveemos hornos de mufla, hornos tubulares, sistemas de Deposición Química de Vapor (CVD) y de Deposición Química de Vapor de plasma mejorado (PECVD), hornos de vacío y hornos de atmosfera, horno dental; que pueden ser hechos a la medida según sus necesidades.',
-    'index_keywords' => 'horno de mufla, horno de tubo, las enfermedades cardiovasculares y Sistemas de PECVD, horno de vacío y atmósfera del horno, horno dental',
-//    /company
-    'company_title' => 'Historia y perfil de empresa',
-    'company_description' => 'La marca Kejia horno es sinónimo de alta calidad, tecnología de calor en el diseño y fabricación de hornos industriales y de laboratorio',
-    'company_keywords' => 'Kejia Historia y perfil de empresa',
-//    /contact-service
+    'index_title' => 'Fabricante y exportador de equipo estético',
+    'index_description' => 'Q-conmutada y serie de láser, luz electrónica y serie del rejuvenecimiento de piel IPL, serie de retiro del pelo del láser del Diodo, Co2 serie de láser fraccionaria y Cryolipolysis, Cavitation vaccum que disuelve la serie gorda etc...',
+//    compañía
+    'company_title' => 'Historia de la compañía & Perfil',
+    'company_description' => 'Incorporamos sus comentarios en nuestra gama de productos, que incluye actualmente el equipo de la reducción de la grasa, el cuerpo de adelgazamiento, la depilación, reducción de arrugas, rejuvenecimiento de la piel, pigmentación y mucho más. ',
+//    contacto-servicio
     'contact_title' => 'Contacto y Servicio',
-    'contact_description' => 'Si necesita horno de laboratorio, por favor, póngase en contacto con nosotros',
-    'contact_keywords' => 'Kejia Horno de Contacto y Servicio',
-//    /downloads
-    'downloads_title' => 'Descargas',
-    'downloads_description' => 'Todos los documentos importantes están disponibles para su descarga aquí.',
-    'downloads_keywords' => 'Kejia horno Descargas',
-//    /downloads/operating
-    'operating_title' => 'Descargas - Operativo',
-    'operating_description' => 'Manuales Kejia Furnace',
-    'operating_keywords' => 'Manuales Kejia Furnace',
-//    /downloads/brochures
-    'brochures_title' => 'Descargas - Los folletos',
-    'brochures_description' => 'Kejia Horno Los folletos',
-    'brochures_keywords' => 'Kejia Horno Los folletos',
-//    /downloads/miscellaneous
-    'miscellaneous_title' => 'Descargas - Varios',
-    'miscellaneous_description' => 'Kejia Horno Misceláneo',
-    'miscellaneous_keywords' => 'Kejia Horno Misceláneo',
-//    /products
-    'products_title' => 'Todos los productos',
-    'products_description' => 'Suministramos horno de mufla, horno de tubo, las enfermedades cardiovasculares y Sistemas de PECVD, horno de vacío y horno de atmósfera, horno dental, y puede ser personalizado de acuerdo a sus necesidades.',
-    'products_keywords' => 'horno de mufla, horno de tubo, las enfermedades cardiovasculares y Sistemas de PECVD, horno de vacío y atmósfera del horno, horno dental.',
-//    /news
+    'contact_description' => 'Si usted necesita el equipo estética,  pongase en contacto con nosotros,por favor',
+//    productos
+    'products_title' => 'Total Productos',
+    'products_description' => 'Proporcionamos Q-conmutada y serie de láser, luz electrónica y serie del rejuvenecimiento de piel IPL, serie de retiro del pelo del láser del Diodo, Co2 serie de láser fraccionaria y Cryolipolysis, Cavitation vaccum que disuelve la serie gorda etc...',
+//    noticias
     'news_title' => 'Noticias',
-    'news_description' => 'Noticias Kejia horno',
-    'news_keywords' => 'Noticias Kejia horno',
-//    /news/cases
-    'cases_title' => 'Custom Cases',
-    'cases_description' => 'Kejia horno Custom Cases',
-    'cases_keywords' => 'Kejia horno Custom Cases',
-
+    'news_description' => 'Centro de Noticia',
 ];
